a471631e3aa7f03029dc77c598ce8fddb1f6f84a
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-13 15:45:15 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
